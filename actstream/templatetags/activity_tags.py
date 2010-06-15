d2ce89e8af5daab811d715778acb7c7fc74ebd22
jordan <jordan@192.168.0.169>
2010-06-15 11:40:03 -0400

    removed debugging

--- actstream/templatetags/activity_tags.py
+++ actstream/templatetags/activity_tags.py
@@ -72,8 +72,8 @@ class DisplayActionShort(Node):
         except:
             user = None
         action_output = re.sub(r'\s+',' ',action_output).strip()
-        if user and user.is_staff:
-            action_output = "<!-- start: %(template)s -->\n%(output)s\n<!-- end: %(template)s -->" % {'template':action_template, 'output':action_output}
+#        if user and user.is_staff:
+#            action_output = "<!-- start: %(template)s -->\n%(output)s\n<!-- end: %(template)s -->" % {'template':action_template, 'output':action_output}
         if self.varname is not None:
             context[self.varname] = action_output
             return ""
