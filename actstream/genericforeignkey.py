393cb749f56bd99ab8c60233972e7025858dcc6b
Ben Slavin <benjamin.slavin@gmail.com>
2012-04-06 12:31:10 -0400

    Backporting GFK code from https://github.com/justquick/django-activity-stream/blob/b0b6382dd0b460166fc2c46e551cdd2d7d0e1ef0/actstream/gfk.py

--- actstream/genericforeignkey.py
+++ actstream/genericforeignkey.py
@@ -1,9 +1,14 @@
-from django.db.models.query import QuerySet
+from django.conf import settings
 from django.db.models import Manager
+from django.db.models.query import QuerySet, EmptyQuerySet
+from django.utils.encoding import smart_unicode
+
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.generic import GenericForeignKey
-from django.conf import settings
-from django.db import models
+
+USE_PREFETCH = getattr(settings, 'USE_PREFETCH', False)
+FETCH_RELATIONS = getattr(settings, 'FETCH_RELATIONS', True)
+
 
 class GFKManager(Manager):
     """
@@ -11,7 +16,11 @@ class GFKManager(Manager):
 
     """
     def get_query_set(self):
-        return GFKQuerySet(self.model)
+        return GFKQuerySet(self.model, using=self.db)
+
+    def none(self):
+        return self.get_query_set().none()
+
 
 class GFKQuerySet(QuerySet):
     """
@@ -22,6 +31,8 @@ class GFKQuerySet(QuerySet):
     Based on http://www.djangosnippets.org/snippets/984/
     Firstly improved at http://www.djangosnippets.org/snippets/1079/
 
+    Extended in django-activity-stream to allow for multi db, text primary keys
+    and empty querysets.
     """
     def __init__(self, *args, **kwargs):
         try:
@@ -31,42 +42,59 @@ class GFKQuerySet(QuerySet):
             self.profile_module = None
         super(GFKQuerySet,self).__init__(*args, **kwargs)
     
-    def fetch_generic_relations(self):
+    
+    def fetch_generic_relations(self, *args):
         qs = self._clone()
 
-        gfk_fields = [g for g in self.model._meta.virtual_fields if isinstance(g, GenericForeignKey)]
-        
-        ct_map = {}
-        item_map = {}
-        data_map = {}
-        
+        if not FETCH_RELATIONS:
+            return qs
+
+        gfk_fields = [g for g in self.model._meta.virtual_fields
+                      if isinstance(g, GenericForeignKey)]
+        if args:
+            gfk_fields = filter(lambda g: g.name in args, gfk_fields)
+
+        if USE_PREFETCH and hasattr(self, 'prefetch_related'):
+            return qs.prefetch_related(*[g.name for g in gfk_fields])
+
+        ct_map, data_map = {}, {}
+
         for item in qs:
             for gfk in gfk_fields:
                 ct_id_field = self.model._meta.get_field(gfk.ct_field).column
-                #print "ct_id=%s" % getattr(item, ct_id_field)
-                #print "item_id=%s" % getattr(item, gfk.fk_field)
-                #print "%s %s" % (ct_id_field, getattr(item, ct_id_field))
-                ct_map.setdefault(
-                    (getattr(item, ct_id_field)), {}
-                    )[getattr(item, gfk.fk_field)] = (gfk.name, item.pk)
-            item_map[item.pk] = item
+                if getattr(item, ct_id_field) is None: continue
+                ct_map.setdefault(getattr(item, ct_id_field), {}
+                    )[smart_unicode(getattr(item, gfk.fk_field))] = (gfk.name,
+                        item.pk)
 
-        for (ct_id), items_ in ct_map.items():
-            if (ct_id):
-                ct = ContentType.objects.get_for_id(ct_id)
-                related_fields = ["user__pk"]
-                if self.profile_module:
-                    related_fields.append("user_%s__pk" % self.profile_module)
+        ctypes = ContentType.objects.using(self.db).in_bulk(ct_map.keys())
+
+        for ct_id, items_ in ct_map.items():
+            if ct_id:
+                ct = ctypes[ct_id]
                 model_class = ct.model_class()
-                id_in = "%s__in" % model_class._meta.pk.name
-                for o in model_class.objects.select_related(*related_fields).filter(**{id_in:items_.keys()}).all():
-                    (gfk_name, item_id) = items_[o.pk]
-                    data_map[(ct_id, o.pk)] = o
+                objects = model_class._default_manager.select_related()
+                for o in objects.filter(pk__in=items_.keys()):
+                    (gfk_name, item_id) = items_[smart_unicode(o.pk)]
+                    data_map[(ct_id, smart_unicode(o.pk))] = o
 
         for item in qs:
             for gfk in gfk_fields:
-                if (getattr(item, gfk.fk_field) != None):
-                    ct_id_field = self.model._meta.get_field(gfk.ct_field).column
-                    setattr(item, gfk.name, data_map[(getattr(item, ct_id_field), getattr(item, gfk.fk_field))])
+                if getattr(item, gfk.fk_field) != None:
+                    ct_id_field = self.model._meta.get_field(gfk.ct_field)\
+                        .column
+                    setattr(item, gfk.name,
+                        data_map[(
+                            getattr(item, ct_id_field),
+                            smart_unicode(getattr(item, gfk.fk_field))
+                        )])
 
         return qs
+
+    def none(self):
+        return self._clone(klass=EmptyGFKQuerySet)
+
+
+class EmptyGFKQuerySet(GFKQuerySet, EmptyQuerySet):
+    def fetch_generic_relations(self):
+        return self
